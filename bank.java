6c69c6bad20eddad1116f30f90f7201d741a25af
judinus10 <judes.20222403@iit.ac.lk>
2024-09-19 14:09:51 +0530

    added constructor

--- bank.java
+++ bank.java
@@ -4,4 +4,13 @@ import java.util.List;
 public class bank {
     private String bankName;
     private List<Account> account ;
+
+    //Constructor
+    public bank (String bankName , List<Account> account){
+        this.bankName = bankName;
+        this.account= new ArrayList<>();
+    }
+
+    
 }
+
