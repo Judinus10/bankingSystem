1460a4bb156ef96791f7089440db767d3c45e08f
judinus10 <judes.20222403@iit.ac.lk>
2024-09-21 12:44:22 +0530

    added constructor of cutomer class

--- customer.java
+++ customer.java
@@ -2,4 +2,9 @@ public class customer {
 
     private int customerId;
     public String name;
+
+    public customer (int customerId, String name){
+        this.customerId=customerId;
+        this.name=name;
+    }
 }
