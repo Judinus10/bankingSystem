6544614e712482c6e650968dec52dd078682717f
judinus10 <judes.20222403@iit.ac.lk>
2024-09-21 13:42:46 +0530

    transfer method added in account class

--- account.java
+++ account.java
@@ -37,4 +37,18 @@ public class account {
         }
         
     }
+
+    public void transfer(account toAccount, double amount){
+        if(amount>0){
+            if(amount<balance){
+                balance-=amount;
+                toAccount.balance+=amount;
+                System.out.println("Transfered "+amount+" from account no "+this.accountNum+" to account "+toAccount.accountNum);
+            }else{
+                System.out.println("Transfer failed due to Insufficient balance");
+            }
+        }else{
+            System.out.println("Transfer failed due to invalid amount");
+        }
+    }
 }
