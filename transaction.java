3539882bc2562d1ae15ffb2323343c013d4c434f
judinus10 <judes.20222403@iit.ac.lk>
2024-09-22 12:06:14 +0530

    Atributes of transaction class added

--- transaction.java
+++ transaction.java
@@ -1,3 +1,8 @@
 public class transaction {
-    
+    private int transactionID;
+    private Date date;
+    private String type;
+    private double amount;
+    private int fromAccNum;
+    private int toAccNum;
 }
