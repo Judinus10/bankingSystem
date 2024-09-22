69477e5daf7d8afd160d45a3485060a0afba06bb
judinus10 <judes.20222403@iit.ac.lk>
2024-09-22 12:09:32 +0530

    constructor for transaction class added

--- transaction.java
+++ transaction.java
@@ -5,4 +5,13 @@ public class transaction {
     private double amount;
     private int fromAccNum;
     private int toAccNum;
+
+    public transaction(int transactionID, Date date, String type, double amount, int fromAccNum, int toAccNum){
+        this.transactionID=transactionID;
+        this.date=date;
+        this.type=type;
+        this.amount=amount;
+        this.fromAccNum=fromAccNum;
+        this.toAccNum=toAccNum;
+    }
 }
