4538407efc41bcd074d9279149394eac09b73212
judinus10 <judes.20222403@iit.ac.lk>
2024-09-23 15:45:21 +0530

    constructor added in checking account class

--- checkingAccount.java
+++ checkingAccount.java
@@ -1,3 +1,8 @@
 public class checkingAccount extends account {
     private double overdraftLimit;
+
+    public checkingAccount(int accountNumber, Customer customer, double initialBalance, double overdraftLimit) {
+        super(accountNumber, customer, initialBalance); // Call the constructor of the Account class
+        this.overdraftLimit = overdraftLimit;
+    }
 }
