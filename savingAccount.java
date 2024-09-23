7ec5f701f24514c56a274de368dc39cab41f6548
judinus10 <judes.20222403@iit.ac.lk>
2024-09-23 15:42:43 +0530

    methods of saving account class added

---
+++ savingAccount.java
@@ -0,0 +1,27 @@
+public class savingAccount extends account  {
+    private double interestRate;
+
+    public SavingsAccount(int accountNum, Customer customer, double balance, double interestRate) {
+        super(accountNum, customer, balance); 
+        this.interestRate = interestRate;
+    }
+
+    public double calculateInterest() {
+        return getBalance() * interestRate;
+    }
+
+    public void applyInterest() {
+        double interest = calculateInterest();
+        deposit(interest); // Add the interest to the account balance
+        System.out.println("Interest of " + interest + " applied to account " + getAccountNumber());
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    
+    }
+}
--- svaingAccount.java
+++
@@ -1,8 +0,0 @@
-public class svaingAccount extends account  {
-    private double interestRate;
-
-    public SavingsAccount(int accountNum, Customer customer, double balance, double interestRate) {
-        super(accountNum, customer, balance); 
-        this.interestRate = interestRate;
-    }
-}
