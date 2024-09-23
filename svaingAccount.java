35feafaf4d95af34985bd6654be41786db024514
judinus10 <judes.20222403@iit.ac.lk>
2024-09-23 15:40:51 +0530

    constructor added

--- svaingAccount.java
+++ svaingAccount.java
@@ -1,3 +1,8 @@
 public class svaingAccount extends account  {
     private double interestRate;
+
+    public SavingsAccount(int accountNum, Customer customer, double balance, double interestRate) {
+        super(accountNum, customer, balance); 
+        this.interestRate = interestRate;
+    }
 }
