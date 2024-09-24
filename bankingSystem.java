a0d7bf1bc45694bac3849c095d123be6aee01127
judinus10 <judes.20222403@iit.ac.lk>
2024-09-24 11:13:17 +0530

    Create customer object

--- bankingSystem.java
+++ bankingSystem.java
@@ -1,5 +1,8 @@
 public class bankingSystem {
     public static void main(String[] args){
         Bank myBank = new Bank("MyBank");
+
+        Customer customer1 = new Customer("John Doe", 101);
+        Customer customer2 = new Customer("Jane Smith", 102);
     }
 }
