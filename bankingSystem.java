26a18f72b5547fda0dfa7a1483f0335c0b71e28c
judinus10 <judes.20222403@iit.ac.lk>
2024-09-24 11:13:45 +0530

    Create accounts for customers

--- bankingSystem.java
+++ bankingSystem.java
@@ -4,5 +4,8 @@ public class bankingSystem {
 
         Customer customer1 = new Customer("John Doe", 101);
         Customer customer2 = new Customer("Jane Smith", 102);
+
+        Account johnAccount = new SavingsAccount(1001, customer1, 500.00, 0.05);
+        Account janeAccount = new CheckingAccount(1002, customer2, 1000.00, 200.00);
     }
 }
